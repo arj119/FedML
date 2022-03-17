9af05c5a99fc903f5efc7c55d2428e74c430a659
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-03-17 22:25:08 +0000

    Started implementation of FedMD

---
+++ fedml_api/standalone/fedmd/utils/data_utils.py
@@ -0,0 +1,14 @@
+def itr_merge(itrs):
+    for itr in itrs:
+        for v in itr:
+            yield v
+
+
+class PublicDataset:
+    def __init__(self, data_loaders):
+        self.data_loaders = data_loaders
+
+    def __iter__(self):
+        return itr_merge(self.data_loaders)
+
+
