66d10b5e4009e6357e05c84e81e62b674ff63cfc
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-03-22 22:15:06 +0000

    Implemented Federated Arjun standalone algorithm

---
+++ fedml_api/standalone/federated_arjun/model_trainer.py
@@ -0,0 +1,212 @@
+import logging
+
+import torch
+from torch import nn
+
+from knowledge_distillation.logits import Logits
+
+try:
+    from fedml_core.trainer.model_trainer import ModelTrainer
+except ImportError:
+    from FedML.fedml_core.trainer.model_trainer import ModelTrainer
+
+
+class FedArjunModelTrainer(ModelTrainer):
+    def __init__(self, adapter_model, local_model):
+        """
+        Args:
+            adapter_model: Homogeneous model between clients that acts as knowledge transfer vehicle.
+            local_model: Heterogeneous model that is chosen by clients that can better utilise client resources
+        """
+        super().__init__(adapter_model)
+        self.adapter_model = adapter_model
+        self.local_model = local_model
+
+    def get_model_params(self):
+        return self.adapter_model.cpu().state_dict()
+
+    def set_model_params(self, model_parameters):
+        self.adapter_model.load_state_dict(model_parameters)
+
+    def train(self, train_data, device, args=None):
+        """
+
+        Args:
+            train_data: Tuple of (train_data, kd_transfer_data).
+            device: Device to perform training on
+            args: Other args
+        Returns:
+
+        """
+        adapter_model, local_model = self.adapter_model.to(device), self.local_model.to(device)
+
+        train_data, kd_transfer_data = train_data
+
+        # train and update assuming classification task
+        kd_criterion = Logits().to(device)
+        cls_criterion = nn.CrossEntropyLoss().to(device)
+
+        if args.client_optimizer == "sgd":
+            optimizer = torch.optim.SGD(self.model.parameters(), lr=args.lr)
+        else:
+            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()), lr=args.lr,
+                                         weight_decay=args.wd, amsgrad=True)
+
+        # 1. Transfer knowledge from adapter model to local model
+        self._knowledge_distillation(adapter_model, local_model, kd_transfer_data, args.kd_epochs, optimizer,
+                                     cls_criterion, kd_criterion, args.kd_lambda, device)
+        # 2. Train local model
+        self._train_loop(local_model, train_data, cls_criterion, args.train_epochs, optimizer, device)
+
+        # 3. Transfer knowledge from local model to adapter model
+        self._knowledge_distillation(local_model, adapter_model, kd_transfer_data, args.kd_epochs, optimizer,
+                                     cls_criterion, kd_criterion, args.kd_lambda, device)
+
+    def _knowledge_distillation(self, teacher_model, student_model, transfer_set, epochs, optimizer, criterion,
+                                kd_criterion, kd_lambda, device):
+        """
+        Args:
+            teacher_model: Teacher model in KD.
+            student_model: Student model that will learn from the teacher.
+            transfer_set: Dataset used to transfer knowledge from the teacher model to the student model.
+            epochs: Number of epochs.
+            optimizer: Optimiser used to update parameters
+            device: Device to perform training on
+            criterion: Task criterion
+            kd_criterion: Knowledge distillation criterion
+            kd_lambda: Knowledge distillation regularisation weight
+        Returns:
+        """
+        teacher_model.eval()
+        student_model.train()
+
+        epoch_loss = []
+        for epoch in range(epochs):
+            batch_loss = []
+            for batch_idx, (x, labels) in enumerate(transfer_set):
+                x, labels = x.to(device), labels.to(device)
+
+                student_model.zero_grad()
+                student_output = student_model(x)  # in classification case will be logits
+                task_loss = criterion(student_output, labels)
+
+                # Knowledge Distillation Loss
+                teacher_output = teacher_model(x).detach()
+                kd_loss = kd_lambda * kd_criterion(teacher_output, student_output)
+
+                loss = task_loss + kd_loss
+
+                loss.backward()
+
+                optimizer.step()
+                batch_loss.append(loss.item())
+            epoch_loss.append(sum(batch_loss) / len(batch_loss))
+            logging.info(f'tEpoch: {epoch}\tLoss: {sum(epoch_loss) / len(epoch_loss):.6f}')
+        return epoch_loss
+
+    def _train_loop(self, model, train_data, criterion, epochs, optimizer, device):
+        """
+
+        Args:
+            model: Model to be trained
+            train_data: Training data
+            criterion: Loss for main task
+            epochs: Epochs of training to be completed
+            optimizer: Optimiser that should be used to update hyperparams
+            device: Device in which training should occur
+        Returns:
+
+        """
+
+        model.train()
+
+        epoch_loss = []
+        for epoch in range(epochs):
+            batch_loss = []
+            for batch_idx, (x, labels) in enumerate(train_data):
+                x, labels = x.to(device), labels.to(device)
+
+                model.zero_grad()
+                output = model(x)  # in classification case will be logits
+                loss = criterion(output, labels)
+                loss.backward()
+                optimizer.step()
+                batch_loss.append(loss.item())
+            epoch_loss.append(sum(batch_loss) / len(batch_loss))
+            logging.info(f'tEpoch: {epoch}\tLoss: {sum(epoch_loss) / len(epoch_loss):.6f}')
+        return epoch_loss
+
+    def get_logits(self, public_data, device):
+        """
+        Calculate logits on public dataset. Part of communication phase
+
+        Args:
+            model: Model to evaluate public dataset on
+            public_data: Public dataset
+
+        Returns:
+
+        """
+        model = self.model
+        model = model.to(device)
+        model.eval()
+
+        logits = []
+        with torch.no_grad():
+            for batch_idx, (x, _) in enumerate(public_data):
+                x = x.to(device)
+                logits.append(model(x))
+        return torch.cat(logits).detach().cpu()
+
+    def test(self, test_data, device, args=None):
+        model = self.local_model.to(device)
+        model.eval()
+
+        metrics = {
+            'test_correct': 0,
+            'test_loss': 0,
+            'test_precision': 0,
+            'test_recall': 0,
+            'test_total': 0
+        }
+
+        '''
+        stackoverflow_lr is the task of multi-label classification
+        please refer to following links for detailed explainations on cross-entropy and corresponding implementation of tff research:
+        https://towardsdatascience.com/cross-entropy-for-classification-d98e7f974451
+        https://github.com/google-research/federated/blob/49a43456aa5eaee3e1749855eed89c0087983541/optimization/stackoverflow_lr/federated_stackoverflow_lr.py#L131
+        '''
+        if args.dataset == "stackoverflow_lr":
+            criterion = nn.BCELoss(reduction='sum').to(device)
+        else:
+            criterion = nn.CrossEntropyLoss().to(device)
+
+        with torch.no_grad():
+            for batch_idx, (x, target) in enumerate(test_data):
+                x = x.to(device)
+                target = target.to(device)
+                pred = model(x)
+                loss = criterion(pred, target)
+
+                if args.dataset == "stackoverflow_lr":
+                    predicted = (pred > .5).int()
+                    correct = predicted.eq(target).sum(axis=-1).eq(target.size(1)).sum()
+                    true_positive = ((target * predicted) > .1).int().sum(axis=-1)
+                    precision = true_positive / (predicted.sum(axis=-1) + 1e-13)
+                    recall = true_positive / (target.sum(axis=-1) + 1e-13)
+                    metrics['test_precision'] += precision.sum().item()
+                    metrics['test_recall'] += recall.sum().item()
+                else:
+                    _, predicted = torch.max(pred, 1)
+                    correct = predicted.eq(target).sum()
+
+                metrics['test_correct'] += correct.item()
+                metrics['test_loss'] += loss.item() * target.size(0)
+                if len(target.size()) == 1:  #
+                    metrics['test_total'] += target.size(0)
+                elif len(target.size()) == 2:  # for tasks of next word prediction
+                    metrics['test_total'] += target.size(0) * target.size(1)
+        return metrics
+
+    def test_on_the_server(self, train_data_local_dict, test_data_local_dict, device, args=None) -> bool:
+        return False
