975f00e76586efdf5f1152ec455108596e002c97
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-04-30 19:03:45 +0100

    Updated image size for cifar10

--- fedml_experiments/standalone/utils/model.py
+++ fedml_experiments/standalone/utils/model.py
@@ -15,7 +15,7 @@ cv_model_builders = {
 }
 
 cv_datasets = {'mnist': 1, 'femnist': 1, 'fed_cifar100': 3, 'cinic10': 3, 'cifar10': 3, 'cifar100': 3}
-cv_datasets_image_size = {'mnist': [1, 28, 28], 'femnist': [1, 28, 28], 'fed_cifar100': [3, 32, 32], 'cinic10': [3, 32, 32], 'cifar10': [3, 32, 32], 'cifar100': [3, 32, 32]}
+cv_datasets_image_size = {'mnist': [1, 28, 28], 'femnist': [1, 28, 28], 'fed_cifar100': [3, 24, 24], 'cinic10': [3, 32, 32], 'cifar10': [3, 32, 32], 'cifar100': [3, 32, 32]}
 
 def create_model(args, model_name, output_dim):
     logging.info("create_model. model_name = %s, output_dim = %s" % (model_name, output_dim))
@@ -47,5 +47,4 @@ def create_model(args, model_name, output_dim):
         model = mobilenet(class_num=output_dim)
     elif args.dataset in cv_datasets:
         model = cv_model_builders[model_name](cv_datasets[args.dataset], output_dim, cv_datasets_image_size[args.dataset])
-
     return model
