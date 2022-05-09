06987bf4b0bcfeece9997c621b415b3dcda85a7b
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-05-10 00:29:22 +0100

    Added generator model for image generation

---
+++ fedml_api/model/cv/generator.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+from abc import ABC
+import torch
+import math
+
+
+class Generator(ABC, nn.Module):
+    def __init__(self, nz=100, img_size=32):
+        super().__init__()
+        self.nz = nz
+        self.img_size = img_size
+
+    def _generate_noise_vector(self, b_size, device):
+        return torch.randn(b_size, self.nz, 1, 1, device=device)
+
+    def generate(self, b_size, device):
+        return self.forward(self._generate_noise_vector(b_size, device))
+
+
+class ImageGenerator(Generator):
+    def __init__(self, nz=100, ngf=64, nc=3, img_size=32):
+        """
+
+        Args:
+            nz: Latent vector size
+            ngf: Number of generator features
+            nc: Number of image channels to produce
+            img_size: Size of the image to produce should be a power of 2 and > 4
+        """
+        super().__init__(nz, img_size)
+
+        self.num_blocks = math.ceil(math.log(img_size // 8, 2))  # Account for first and last blocks
+
+        self.main = nn.Sequential(
+            # Input: N x latent_vector_size x 1 x 1
+            self._block(nz, ngf * (2 ** self.num_blocks), 4, 1, 0),  # img: 4x4
+        )
+
+        for i in range(self.num_blocks):
+            num_features = ngf * (2 ** (self.num_blocks - i))
+            self.main.add_module(f'block {i}', self._block(num_features, num_features // 2, 4, 2, 1))
+
+        self.main.add_module('end', nn.Sequential(
+            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh(), ))
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels, out_channels, kernel_size, stride, padding, bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True),
+        )
+
+    def forward(self, noise):
+        return self.main(noise)
