9fe4e8e55e3dad49a26c79f1a20650d421bd1f33
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-05-20 00:27:36 +0100

    Refactored existing dataloaders to use new method

--- fedml_api/data_preprocessing/cifar10/data_loader.py
+++ fedml_api/data_preprocessing/cifar10/data_loader.py
@@ -7,51 +7,12 @@ import torchvision.transforms as transforms
 
 from .datasets import CIFAR10_truncated
 
-#logging.basicConfig()
+# logging.basicConfig()
+from ..utils.partition import get_partition_indices_train, get_partition_indices_test, load_partition_data, \
+    load_partition_data_distributed
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-
-# generate the non-IID distribution for all methods
-def read_data_distribution(filename='./data_preprocessing/non-iid-distribution/CIFAR10/distribution.txt'):
-    distribution = {}
-    with open(filename, 'r') as data:
-        for x in data.readlines():
-            if '{' != x[0] and '}' != x[0]:
-                tmp = x.split(':')
-                if '{' == tmp[1].strip():
-                    first_level_key = int(tmp[0])
-                    distribution[first_level_key] = {}
-                else:
-                    second_level_key = int(tmp[0])
-                    distribution[first_level_key][second_level_key] = int(tmp[1].strip().replace(',', ''))
-    return distribution
-
-
-def read_net_dataidx_map(filename='./data_preprocessing/non-iid-distribution/CIFAR10/net_dataidx_map.txt'):
-    net_dataidx_map = {}
-    with open(filename, 'r') as data:
-        for x in data.readlines():
-            if '{' != x[0] and '}' != x[0] and ']' != x[0]:
-                tmp = x.split(':')
-                if '[' == tmp[-1].strip():
-                    key = int(tmp[0])
-                    net_dataidx_map[key] = []
-                else:
-                    tmp_array = x.split(',')
-                    net_dataidx_map[key] = [int(i.strip()) for i in tmp_array]
-    return net_dataidx_map
-
-
-def record_net_data_stats(y_train, net_dataidx_map):
-    net_cls_counts = {}
-
-    for net_i, dataidx in net_dataidx_map.items():
-        unq, unq_cnt = np.unique(y_train[dataidx], return_counts=True)
-        tmp = {unq[i]: unq_cnt[i] for i in range(len(unq))}
-        net_cls_counts[net_i] = tmp
-    logging.debug('Data statistics: %s' % str(net_cls_counts))
-    return net_cls_counts
 
 
 class Cutout(object):
@@ -110,56 +71,6 @@ def load_cifar10_data(datadir):
     return (X_train, y_train, X_test, y_test)
 
 
-def partition_data(dataset, datadir, partition, n_nets, alpha):
-    logging.info("*********partition data***************")
-    X_train, y_train, X_test, y_test = load_cifar10_data(datadir)
-    n_train = X_train.shape[0]
-    # n_test = X_test.shape[0]
-
-    if partition == "homo":
-        total_num = n_train
-        idxs = np.random.permutation(total_num)
-        batch_idxs = np.array_split(idxs, n_nets)
-        net_dataidx_map = {i: batch_idxs[i] for i in range(n_nets)}
-
-    elif partition == "hetero":
-        min_size = 0
-        K = 10
-        N = y_train.shape[0]
-        logging.info("N = " + str(N))
-        net_dataidx_map = {}
-
-        while min_size < 10:
-            idx_batch = [[] for _ in range(n_nets)]
-            # for each class in the dataset
-            for k in range(K):
-                idx_k = np.where(y_train == k)[0]
-                np.random.shuffle(idx_k)
-                proportions = np.random.dirichlet(np.repeat(alpha, n_nets))
-                ## Balance
-                proportions = np.array([p * (len(idx_j) < N / n_nets) for p, idx_j in zip(proportions, idx_batch)])
-                proportions = proportions / proportions.sum()
-                proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
-                idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
-                min_size = min([len(idx_j) for idx_j in idx_batch])
-
-        for j in range(n_nets):
-            np.random.shuffle(idx_batch[j])
-            net_dataidx_map[j] = idx_batch[j]
-
-    elif partition == "hetero-fix":
-        dataidx_map_file_path = './data_preprocessing/non-iid-distribution/CIFAR10/net_dataidx_map.txt'
-        net_dataidx_map = read_net_dataidx_map(dataidx_map_file_path)
-
-    if partition == "hetero-fix":
-        distribution_file_path = './data_preprocessing/non-iid-distribution/CIFAR10/distribution.txt'
-        traindata_cls_counts = read_data_distribution(distribution_file_path)
-    else:
-        traindata_cls_counts = record_net_data_stats(y_train, net_dataidx_map)
-
-    return X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts
-
-
 # for centralized training
 def get_dataloader(dataset, datadir, train_bs, test_bs, dataidxs=None):
     return get_dataloader_CIFAR10(datadir, train_bs, test_bs, dataidxs)
@@ -200,70 +111,25 @@ def get_dataloader_test_CIFAR10(datadir, train_bs, test_bs, dataidxs_train=None,
 
 def load_partition_data_distributed_cifar10(process_id, dataset, data_dir, partition_method, partition_alpha,
                                             client_number, batch_size):
-    X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts = partition_data(dataset,
-                                                                                             data_dir,
-                                                                                             partition_method,
-                                                                                             client_number,
-                                                                                             partition_alpha)
-    class_num = len(np.unique(y_train))
-    logging.info("traindata_cls_counts = " + str(traindata_cls_counts))
-    train_data_num = sum([len(net_dataidx_map[r]) for r in range(client_number)])
-
-    # get global test data
-    if process_id == 0:
-        train_data_global, test_data_global = get_dataloader(dataset, data_dir, batch_size, batch_size)
-        logging.info("train_dl_global number = " + str(len(train_data_global)))
-        logging.info("test_dl_global number = " + str(len(test_data_global)))
-        train_data_local = None
-        test_data_local = None
-        local_data_num = 0
-    else:
-        # get local dataset
-        dataidxs = net_dataidx_map[process_id - 1]
-        local_data_num = len(dataidxs)
-        logging.info("rank = %d, local_sample_number = %d" % (process_id, local_data_num))
-        # training batch size = 64; algorithms batch size = 32
-        train_data_local, test_data_local = get_dataloader(dataset, data_dir, batch_size, batch_size,
-                                                 dataidxs)
-        logging.info("process_id = %d, batch_num_train_local = %d, batch_num_test_local = %d" % (
-            process_id, len(train_data_local), len(test_data_local)))
-        train_data_global = None
-        test_data_global = None
-    return train_data_num, train_data_global, test_data_global, local_data_num, train_data_local, test_data_local, class_num
+    load_partition_data_distributed(data_dir=data_dir, dataset=load_cifar10_data(data_dir),
+                                    global_dataloaders=get_dataloader(dataset, data_dir, batch_size, batch_size),
+                                    get_dataloader_test=get_dataloader_test,
+                                    process_id=process_id,
+                                    partition_alpha=partition_alpha,
+                                    partition_method=partition_method,
+                                    client_number=client_number,
+                                    batch_size=batch_size
+                                    )
 
 
-def load_partition_data_cifar10(dataset, data_dir, partition_method, partition_alpha, client_number, batch_size, silo_proc_num=0):
-    X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts = partition_data(dataset,
-                                                                                             data_dir,
-                                                                                             partition_method,
-                                                                                             client_number,
-                                                                                             partition_alpha)
-    class_num = len(np.unique(y_train))
-    logging.info("traindata_cls_counts = " + str(traindata_cls_counts))
-    train_data_num = sum([len(net_dataidx_map[r]) for r in range(client_number)])
-
-    train_data_global, test_data_global = get_dataloader(dataset, data_dir, batch_size, batch_size)
-    logging.info("train_dl_global number = " + str(len(train_data_global)))
-    logging.info("test_dl_global number = " + str(len(test_data_global)))
-    test_data_num = len(test_data_global)
-
-    # get local dataset
-    data_local_num_dict = dict()
-    train_data_local_dict = dict()
-    test_data_local_dict = dict()
-
-    for client_idx in range(client_number):
-        dataidxs = net_dataidx_map[client_idx]
-        local_data_num = len(dataidxs)
-        data_local_num_dict[client_idx] = local_data_num
-        logging.info("client_idx = %d, local_sample_number = %d" % (client_idx, local_data_num))
-
-        # training batch size = 64; algorithms batch size = 32
-        train_data_local, test_data_local = get_dataloader(dataset, data_dir, batch_size, batch_size,
-                                                 dataidxs)
-        logging.info("client_idx = %d, batch_num_train_local = %d, batch_num_test_local = %d" % (
-            client_idx, len(train_data_local), len(test_data_local)))
-        train_data_local_dict[client_idx] = train_data_local
-        test_data_local_dict[client_idx] = test_data_local
-    return train_data_num, test_data_num, train_data_global, test_data_global, \
-           data_local_num_dict, train_data_local_dict, test_data_local_dict, class_num
+def load_partition_data_cifar10(dataset, data_dir, partition_method, partition_alpha, client_number, batch_size,
+                                silo_proc_num=0):
+    return load_partition_data(data_dir, dataset=load_cifar10_data(data_dir),
+                               global_dataloaders=get_dataloader(dataset, data_dir, batch_size, batch_size),
+                               get_dataloader_test=get_dataloader_test,
+                               partition_method=partition_method,
+                               partition_alpha=partition_alpha,
+                               client_number=client_number,
+                               batch_size=batch_size,
+                               silo_proc_num=silo_proc_num
+                               )
