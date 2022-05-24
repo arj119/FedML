82085aa7f63f58bca8564d9876a78a6690fd57ec
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-05-24 23:24:25 +0100

    Implemented baseline method that trains all local models privately

---
+++ fedml_api/standalone/baseline/client.py
@@ -0,0 +1,44 @@
+import logging
+
+from torch.utils.data import random_split, DataLoader, TensorDataset
+
+from fedml_api.standalone.federated_arjun.model_trainer import FedArjunModelTrainer
+from fedml_api.standalone.utils.BaseClient import BaseClient
+
+
+class FedArjunClient(BaseClient):
+
+    def __init__(self, client_idx, local_training_data, local_test_data, local_sample_number, global_test_data, args,
+                 device,
+                 model_trainer: FedArjunModelTrainer):
+        super().__init__(client_idx, local_training_data, local_test_data, local_sample_number, global_test_data, args,
+                         device,
+                         model_trainer)
+
+        # self.local_training_data, sizes = self.prepare_local_training_data(local_training_data)
+        self.local_training_data = local_training_data
+
+        logging.info("self.local_sample_number = " + str(self.local_sample_number))
+        logging.info(f"""
+            ########### Client {client_idx}: ###############
+            Local sample number: {self.local_sample_number}
+            Training dataset size: {len(local_training_data)}
+            Test dataset size: {len(local_test_data)}
+            ################################################
+        """)
+
+    def train(self, w_global=None):
+        logging.info(f'### Training Client {self.client_idx} ###')
+        self.model_trainer.train(self.local_training_data, self.device, self.args)
+        logging.info(f'### Training Client {self.client_idx} (complete) ###')
+
+    def pre_train(self):
+        """
+
+        Args:
+            public_data: Public dataset used for transfer learning
+
+        Returns:
+
+        """
+        self.model_trainer.pre_train(private_data=self.local_training_data, device=self.device, args=self.args)
