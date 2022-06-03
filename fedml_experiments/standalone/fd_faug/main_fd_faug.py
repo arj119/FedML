e9a38e884960d36849b5d82fca08fc467797f06d
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-06-03 17:50:43 +0100

    Updated FD

--- fedml_experiments/standalone/fd_faug/main_fd_faug.py
+++ fedml_experiments/standalone/fd_faug/main_fd_faug.py
@@ -1,16 +1,14 @@
-import logging
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../")))
 
-from fedml_experiments.standalone.utils.model import create_local_models_from_config
 from fedml_api.standalone.fd_faug.FD_FAug_api import FDFAugAPI
 from fedml_experiments.standalone.utils.experiment import ExperimentBase
 
 
 class FDFAugExperiment(ExperimentBase):
-    algorithm_name = 'FD_FAug'
+    algorithm_name = 'FD'
 
     def add_custom_args(self, parser):
         """
@@ -18,7 +16,7 @@ class FDFAugExperiment(ExperimentBase):
         return a parser added with args required by fit
         """
         # FD FAug arguments
-        parser.add_argument('--kd_gamma', type=float, default=1.,
+        parser.add_argument('--kd_gamma', type=float, default=0.8,
                             help='Weighting of knowledge distillation regularisation term')
 
         parser.add_argument('--client_config_file', type=str,
