cd245652610afbc6958bf8dfbdd1072254c2ccc6
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2022-06-06 12:18:35 +0100

    Implemented fd faug with proper gan augmentation

--- fedml_experiments/standalone/fd_faug/main_fd_faug.py
+++ fedml_experiments/standalone/fd_faug/main_fd_faug.py
@@ -4,13 +4,13 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../")))
 
-from fedml_experiments.standalone.utils.model import create_local_models_from_config
+from fedml_experiments.standalone.utils.model import create_local_models_from_config, create_model
 from fedml_api.standalone.fd_faug.FD_FAug_api import FDFAugAPI
 from fedml_experiments.standalone.utils.experiment import ExperimentBase
 
 
 class FDFAugExperiment(ExperimentBase):
-    algorithm_name = 'FD_FAug with Augmentation'
+    algorithm_name = 'FD_FAug with GAN Augmentation'
 
     def add_custom_args(self, parser):
         """
@@ -18,7 +18,7 @@ class FDFAugExperiment(ExperimentBase):
         return a parser added with args required by fit
         """
         # FD FAug arguments
-        parser.add_argument('--kd_gamma', type=float, default=1.,
+        parser.add_argument('--kd_gamma', type=float, default=0.8,
                             help='Weighting of knowledge distillation regularisation term')
 
         parser.add_argument('--client_config_file', type=str,
@@ -28,10 +28,30 @@ class FDFAugExperiment(ExperimentBase):
 
         parser.add_argument('--share_percentage', type=float, default=0.05,
                             help='Number of epochs to be carries out on private dataset in local training')
+
+        parser.add_argument('--nz', type=int,
+                            default=100,
+                            help='Latent vector size')
+
+        parser.add_argument('--ngf', type=int,
+                            default=100,
+                            help='Number of generator features')
+
+        parser.add_argument('--gen_lr', type=float, default=0.001,
+                            help='Learning rate of generator')
+
+        parser.add_argument('--gen_optimizer', type=str, default='adam',
+                            help='Optimiser of generator')
+
+        parser.add_argument('--faug_epochs', type=int,
+                            default=100,
+                            help='Number of FAug training epochs')
         return parser
 
     def experiment_start(self, client_model_config, client_models, args, device, dataset):
-        api = FDFAugAPI(dataset, device, args, client_models)
+        generator = create_model(args, model_name=client_model_config['generator'], output_dim=dataset[7])
+        discriminator = create_model(args, model_name=client_model_config['discriminator'], output_dim=dataset[7])
+        api = FDFAugAPI(dataset, device, args, generator, discriminator, client_models)
         api.train()
 
 
